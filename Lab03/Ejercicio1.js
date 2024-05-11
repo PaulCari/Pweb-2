50b050b1b8fec109cf78f59b5a44d4d0d92064a8
PaulCari <pcaril@unsa.edu.pe>
2024-05-10 21:56:57 -0500

    Añadimos el ejercicio 1

---
+++ Lab03/Ejercicio1.js
@@ -0,0 +1,30 @@
+
+/* Ejercicio 1
+ Crear una variable llamada carnName y asignarle el valor Volvo*/
+let carName = "Volvo";
+
+/* Ejercicio 2
+ Crear una variable llamada x y asignarle el valor 50*/
+let x = 50;
+
+/* Ejercicio 3
+ Mostrar la suma de 5 + 10, usando las variables x e y*/
+let x = 5;
+let y = 10;
+document.getElementById("demo").innerHTML = x + 10;
+
+
+ /* Ejercicio 4
+ Cree una variable llamada z, asígnele x + y, muestre el resultado en un cuadro de alerta.*/
+ let x = 5;
+ let y = 10;
+ let z = 15;
+ alert(z);
+
+/* Ejercicio 5
+En una sola línea, declara tres variables con los siguientes nombres y valores:
+firstName = "John"
+lastName = "Doe"
+age = 35
+*/
+let firstName = "John", lastName = "Doe", age = 35;
